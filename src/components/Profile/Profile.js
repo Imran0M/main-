7cc7bc4cacb79368bf3df5bce104831100879121
MohamedimranM <mohamedimranm04@gmail.com>
2023-06-14 12:39:27 +0530

    hellos

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,7 +8,7 @@ function Profile() {
             <div className=" profile container">
                 <div className="img-name">
                     <img className="my-image " src={imran_m} alt="imran" />
-                    <p className="my-name ">Mohamed Imran</p>
+                    <p className="my-name ">Mohamed Imran M</p>
                 </div>
                 <div className="tittle-text">
                     <p className="about-text">I am Full stack web Developer with a year of experience in building responsive websites and web application development. Strong proficiency in HTML, CSS, and JavaScript, Experience with popular front-end library such as ReactJS, Strong understanding of problem solving and web standards and best practices for web development.</p>
